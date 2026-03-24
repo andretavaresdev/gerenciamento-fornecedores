5407c08d000f103a3aefe1e07399d0925e139f1b
Andre Tavares <andreetavaress1@gmail.com>
2026-03-23 22:16:13 -0300

    add: classe Fornecedor criada

---
+++ src/main/java/com/tavares/gerenciamentofornecedores/GerenciamentofornecedoresApplication.java
@@ -0,0 +1,13 @@
+package com.tavares.gerenciamentofornecedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentofornecedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentofornecedoresApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tavares/gerenciamentofornecedores/models/Fornecedor.java
@@ -0,0 +1,53 @@
+package com.tavares.gerenciamentofornecedores.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_fornecedor")
+public class Fornecedor {
+    @Id
+    @GeneratedValue
+    private String id;
+
+    @Column(name="apelido")
+    private String nomeFantasia;
+
+    private String cnpj;
+
+    @Column(name="contato_fornecedor")
+    private String contato;
+
+    public Fornecedor(){}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
